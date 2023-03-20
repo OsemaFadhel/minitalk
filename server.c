311e3d9808569a787f9aa8c2b883d4b9a84c6c61
KingFazzel <osemafadhel41@gmail.com>
2023-03-20 18:43:58 -0400

    minitalk

--- server.c
+++ server.c
@@ -6,8 +6,23 @@
 /*   By: ofadhel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/20 09:57:32 by ofadhel           #+#    #+#             */
-/*   Updated: 2023/03/20 09:57:34 by ofadhel          ###   ########.fr       */
+/*   Updated: 2023/03/20 18:43:45 by ofadhel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <stdio.h>
+#include "minitalk.h"
 
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 1)
+	{
+		printf("ERROR, No arguments needed!\n";
+		return ;
+	}
+	pid = getpid();
+	printf("%d\n", pid);
+}
